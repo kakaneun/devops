bcf8f59ce4dd689a2dc7b4a323b4a8083fd75509
kakaneun <kakaneun@gmail.com>
2025-09-12 22:11:25 -0300

    update main.py
    modifiquei a lógica de um dos end points

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+import random
+
+app = FastAPI()
+
+# http://127.0.0.1:8000
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+# http://127.0.0.1:8000/teste
+@app.get("/teste")
+async def funcaoteste():
+    return {"teste": "ok", "num_aleatorio": random.randint(0, 20000)}
